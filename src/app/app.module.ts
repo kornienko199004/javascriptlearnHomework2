37f1f917ba2a4e4d54d942d7457840ea8971da06
Корниенко Алексей Александрович <Kornienko-A@gis.lan>
2019-07-17 17:33:11 +0300

    Add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatCardModule} from '@angular/material/card';
 
 import { AppComponent } from './app.component';
+import { MainCardComponent } from './main-card/main-card.component';
+import { WeatherCardComponent } from './weather-card/weather-card.component';
+import { SocialCardComponent } from './social-card/social-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainCardComponent,
+    WeatherCardComponent,
+    SocialCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatGridListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
