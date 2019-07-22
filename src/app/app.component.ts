9f80b68c2231102353f9b3897a1b3a6493fe3f50
Корниенко Алексей Александрович <Kornienko-A@gis.lan>
2019-07-22 17:14:03 +0300

    Some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -32,6 +32,44 @@ export class AppComponent {
       phone: 89112223301,
       weather: {
          title: 'Солнечно',
+         icon: '../assets/img/sun.svg',
+         water: 15,
+         temperature: 20,
+      },
+      social_info: {
+         title: 'Instagram',
+         // tslint:disable-next-line:max-line-length
+         img: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn-st1.rtr-vesti.ru%2Fp%2Fxw_1446621.jpg&imgrefurl=https%3A%2F%2Fwww.vesti.ru%2Fdoc.html%3Fid%3D3132347&docid=9E_6B-C8Vq25WM&tbnid=ONNeCxWYN8JFgM%3A&vet=10ahUKEwjtv4SSwrvjAhVlkIsKHZYyBU0QMwhVKAMwAw..i&w=720&h=409&bih=881&biw=1745&q=instagram&ved=0ahUKEwjtv4SSwrvjAhVlkIsKHZYyBU0QMwhVKAMwAw&iact=mrc&uact=8',
+         followers: 1000,
+         following: 90,
+      },
+      type: 'Москва',
+    },
+    {
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Moscow-City2015.jpg/405px-Moscow-City2015.jpg',
+      address: 'РФ Москва столица',
+      phone: 89112223301,
+      weather: {
+         title: 'Солнечно',
+         icon: '',
+         water: 15,
+         temperature: 20,
+      },
+      social_info: {
+         title: 'Instagram',
+         // tslint:disable-next-line:max-line-length
+         img: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fcdn-st1.rtr-vesti.ru%2Fp%2Fxw_1446621.jpg&imgrefurl=https%3A%2F%2Fwww.vesti.ru%2Fdoc.html%3Fid%3D3132347&docid=9E_6B-C8Vq25WM&tbnid=ONNeCxWYN8JFgM%3A&vet=10ahUKEwjtv4SSwrvjAhVlkIsKHZYyBU0QMwhVKAMwAw..i&w=720&h=409&bih=881&biw=1745&q=instagram&ved=0ahUKEwjtv4SSwrvjAhVlkIsKHZYyBU0QMwhVKAMwAw&iact=mrc&uact=8',
+         followers: 1000,
+         following: 90,
+      },
+      type: 'Москва',
+    },
+    {
+      img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a2/Moscow-City2015.jpg/405px-Moscow-City2015.jpg',
+      address: 'РФ Москва столица',
+      phone: 89112223301,
+      weather: {
+         title: 'Солнечно',
          icon: '',
          water: 15,
          temperature: 20,
--- src/app/main-card/main-card.component.ts
+++ src/app/main-card/main-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-main-card',
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main-card.component.scss']
 })
 export class MainCardComponent implements OnInit {
+  @Input() data: any[];
+  index = 0;
 
   constructor() { }
 
   ngOnInit() {
+    console.log(this.data);
   }
 
 }
